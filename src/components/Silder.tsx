2883ea20bc83e3c5cd32b348cf3ca882f8d0d6d1
vijaykumarchakka00 <107754996+vijaykumarchakka00@users.noreply.github.com>
2025-11-12 00:01:31 +0530

    commit

--- src/components/Silder.tsx
+++ src/components/Silder.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import '../index.css';
 
 interface CarouselProps {
   items: React.ReactNode[]; // array of JSX (images, text, etc.)
